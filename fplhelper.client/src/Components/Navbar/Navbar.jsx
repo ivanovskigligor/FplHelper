8d04e4e9fc8aee02fc72ee736a392deacd9c0c3c
Gligor Ivanovski <ivanovskigligor@gmail.com>
2025-01-23 10:18:42 +0100

    Update Navbar.jsx
    test

--- fplhelper.client/src/Components/Navbar/Navbar.jsx
+++ fplhelper.client/src/Components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
                         aria-controls="mobile-menu"
                         aria-expanded="false"
                     >
-                        <span className="sr-only">Open main menu</span>
+                        <span className="sr-only">Open menu</span>
                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
